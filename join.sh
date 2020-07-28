17732b81e02d0e7eec1ab5ad069d0cc72a6412ab
Ashish Maurya <ashmaurya7985@gmail.com>
2020-07-28 15:31:57 +0530

    Update join.sh

--- join.sh
+++ join.sh
@@ -1,2 +1,2 @@
-kubeadm join 192.168.0.105:6443 --token k5v3ue.amxxmfmyfxaq8w89 \
-    --discovery-token-ca-cert-hash sha256:a488da89400676d47d64c6e9c443921ad9433231856b2f6a98ec6572256bd345 
+kubeadm join 192.168.0.105:6443 --token hscj0q.uax62xysg7zv97f7 \
+    --discovery-token-ca-cert-hash sha256:836b3283c136c1a050536d96342ddeb68ff0d630a9ce9f4ea8e7d2bd59fceff1 
